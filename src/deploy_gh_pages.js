847c844acad3e3f2101fe0a5282caa9cf3b54abd
Juho Vepsalainen <bebraw@gmail.com>
2015-08-19 13:21:38 +0300

    Fix deployment script
    Apparently requiring config nukes `./dist`...

--- src/deploy_gh_pages.js
+++ src/deploy_gh_pages.js
@@ -2,10 +2,11 @@
 
 var ghpages = require('gh-pages');
 
-var config = require('../webpack.config');
+// XXX: destroys ./dist!
+//var config = require('../webpack.config');
 
 main();
 
 function main() {
-  ghpages.publish(config.output.path, console.error.bind(console));
+  ghpages.publish('./dist', console.error.bind(console));
 }
