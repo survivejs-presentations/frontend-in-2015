1e83c8a52139922e797bef148ac9cbcbeb2d6084
Juho Vepsalainen <bebraw@gmail.com>
2015-08-21 12:04:42 +0300

    Add npm section

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -10,6 +10,7 @@ import preloader from "../src/utils/preloader";
 const images = {
   city: require("./city.jpg"),
   cheese: require("./cheese.jpg"),
+  boxing: require("./boxing.jpg"),
 };
 
 preloader([images.city, images.cheese]);
@@ -264,6 +265,37 @@ export default class extends React.Component {
           <CodePane
             lang="html"
             source={require("raw!./examples/ractive.example")}
+            margin="20px auto"/>
+        </Slide>
+
+        {/* package managers */}
+        <Slide transition={["slide", "spin"]} bgColor="primary">
+          <Heading caps fit size={1} textColor="tertiary">
+            Package Managers
+          </Heading>
+          <Heading caps size={2} textColor="secondary">
+            To keep things together
+          </Heading>
+        </Slide>
+        <Slide transition={["zoom", "fade"]} bgColor="primary" bgImage={images.boxing.replace("/", "")} bgDarken={0.75}>
+          <Heading caps >NPM</Heading>
+          <Heading caps size={2} textColor="primary">Wins</Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <List>
+            <ListItem><Appear fid="1">176k+ packages and growing fast</Appear></ListItem>
+            <ListItem><Appear fid="2">Dependency management</Appear></ListItem>
+            <ListItem><Appear fid="3">Package authoring</Appear></ListItem>
+            <ListItem><Appear fid="4">Both front-end/back-end</Appear></ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['zoom', 'fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          <Heading size={1} textColor="tertiary">
+            package.json
+          </Heading>
+          <CodePane
+            lang="json"
+            source={require("raw!./examples/npm.example")}
             margin="20px auto"/>
         </Slide>
 
