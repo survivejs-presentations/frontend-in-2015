8ec7c9cea67d82ee3425392e72730541d65db3bf
Juho Vepsalainen <bebraw@gmail.com>
2015-08-21 18:08:52 +0300

    Add framework and library comparisons

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -2,7 +2,8 @@ import React from "react/addons";
 
 import {
   Appear, BlockQuote, Cite, CodePane, Deck, Fill,
-  Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text
+  Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text,
+  Table, TableRow, TableHeaderItem, TableItem
 } from "../src/spectacle";
 
 import preloader from "../src/utils/preloader";
@@ -103,32 +104,6 @@ export default class extends React.Component {
             </Fill>
           </Layout>
         </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Layout>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Frameworks
-              </Heading>
-              <List>
-                <ListItem><Appear fid="1">Solve a lot out of box</Appear></ListItem>
-                <ListItem><Appear fid="2">Great esp. for prototyping/small/medium apps</Appear></ListItem>
-                <ListItem><Appear fid="3">Come with limits</Appear></ListItem>
-                <ListItem><Appear fid="4">What about evolution?</Appear></ListItem>
-              </List>
-            </Fill>
-            <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Libraries
-              </Heading>
-              <List>
-                <ListItem><Appear fid="5">Solve some specific problem</Appear></ListItem>
-                <ListItem><Appear fid="6">Great for evolving apps</Appear></ListItem>
-                <ListItem><Appear fid="7">Limited by architectural skill</Appear></ListItem>
-                <ListItem><Appear fid="8">Replace library at a time as they become obsolete</Appear></ListItem>
-              </List>
-            </Fill>
-          </Layout>
-        </Slide>
 
         {/* frameworks */}
         <Slide transition={["slide", "spin"]} bgColor="primary">
@@ -211,6 +186,39 @@ export default class extends React.Component {
             source={require("raw!./examples/meteor.example")}
             margin="20px auto"/>
         </Slide>
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+          <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+            Comparison
+          </Heading>
+          <Layout>
+            <Table>
+              <TableRow>
+                <TableHeaderItem>Framework</TableHeaderItem>
+                <TableHeaderItem>Description</TableHeaderItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>AngularJS</TableItem>
+                <TableItem>Google backed, full featured, v2 in sight</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Ember.js</TableItem>
+                <TableItem>Niche, popular with rubists</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Backbone.js</TableItem>
+                <TableItem>Used to be popular, still in use</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Knockout.js</TableItem>
+                <TableItem>MVVM, niche</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Meteor</TableItem>
+                <TableItem>Both front-end and back-end</TableItem>
+              </TableRow>
+            </Table>
+          </Layout>
+        </Slide>
 
         {/* libraries */}
         <Slide transition={["slide", "spin"]} bgColor="primary">
@@ -238,6 +246,11 @@ export default class extends React.Component {
             </Heading>
           </Appear>
           <Appear fid="4">
+            <Heading size={2} caps textColor="primary">
+              Cycle.js
+            </Heading>
+          </Appear>
+          <Appear fid="5">
             <Heading size={2} caps textColor="tertiary">
               + 100 000 more
             </Heading>
@@ -269,6 +282,44 @@ export default class extends React.Component {
             lang="html"
             source={require("raw!./examples/ractive.example")}
             margin="20px auto"/>
+        </Slide>
+        <Slide transition={['zoom', 'fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+          <Heading caps size={1} textColor="tertiary">
+            Cycle.js
+          </Heading>
+          <CodePane
+            lang="javascript"
+            source={require("raw!./examples/cycle.example")}
+            margin="20px auto"/>
+        </Slide>
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+          <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+            Comparison
+          </Heading>
+          <Layout>
+            <Table>
+              <TableRow>
+                <TableHeaderItem>Library</TableHeaderItem>
+                <TableHeaderItem>Description</TableHeaderItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>React.js</TableItem>
+                <TableItem>Backed by Facebook, strong ecosystem</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Vue.js</TableItem>
+                <TableItem>Niche alternative</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Ractive.js</TableItem>
+                <TableItem>Reactive templates by The Guardian</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>Cycle.js</TableItem>
+                <TableItem>Reactive newcomer</TableItem>
+              </TableRow>
+            </Table>
+          </Layout>
         </Slide>
 
         {/* package managers */}
